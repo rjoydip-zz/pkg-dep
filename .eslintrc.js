e23fa2ea872fe56049c2d381c7d6239eb2221ccf
Joydip Roy <joydipand@gmail.com>
2018-07-02 22:26:47 +0545

    eslint removed ecmaFeatures

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,15 @@
 module.exports = {
-    "extends": ["standard"],
+    "parserOptions": {
+        "ecmaVersion": 6
+    },
     "env": {
         "browser": true,
         "node": true,
         "es6": true,
         "jest": true
     },
-    rules: {
+    "rules": {
         "quotes": ["error", "single"],
         "indent": ["error", 2]
-    },
-    "parserOptions": {
-        "ecmaVersion": 6
-    },
-    "ecmaFeatures": {
-        "arrowFunctions": true,
-        "binaryLiterals": true,
-        "blockBindings": true,
-        "classes": true,
-        "defaultParams": true,
-        "destructuring": true,
-        "forOf": true,
-        "generators": true,
-        "modules": true,
-        "objectLiteralComputedProperties": true,
-        "objectLiteralDuplicateProperties": true,
-        "objectLiteralShorthandMethods": true,
-        "objectLiteralShorthandProperties": true,
-        "octalLiterals": true,
-        "regexUFlag": true,
-        "regexYFlag": true,
-        "spread": true,
-        "superInFunctions": true,
-        "templateStrings": true,
-        "unicodeCodePointEscapes": true,
-        "globalReturn": true,
-        "jsx": true
     }
 };
